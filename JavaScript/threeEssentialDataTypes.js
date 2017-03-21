e28c2d4dcc2433eb13b18055f52ffddd77a5e771
Lumish <mbretallen@gmail.com>
2017-03-21 15:29:56 -0400

    init

---
+++ JavaScript/threeEssentialDataTypes.js
@@ -0,0 +1,18 @@
+/*
+Just as a language has nouns, verbs, etc., JavaScript has its own building blocks; developers call them data types.
+There are three essential data types to know for now:
+1. String: Any grouping of words or numbers surrounded by single quotes: ' ... ' or double quotes: " ... ".
+2. Number: Any number, including numbers with decimals, without quotes: 4, 8, 1516, 23.42.
+3. Boolean: This is always one two words. Either true or false, with no quotations.
+*/
+
+var myString = 'string';
+var myNumber = 37;
+var myBoolean = false;
+
+
+// Do not edit the code under this line
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+
