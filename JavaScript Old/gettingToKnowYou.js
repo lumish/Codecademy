4b8135cf17a150d7bbb6062c47b50539b2457267
Lumish <mbretallen@gmail.com>
2017-03-21 20:58:23 -0400

    init

---
+++ JavaScript Old/gettingToKnowYou.js
@@ -0,0 +1,20 @@
+//Lesson 1
+"Bret"
+
+//Lesson 2
+"Bret".length
+
+//Lesson 3
+3 + 4
+
+//Lesson 4
+3 * 85
+
+//Lesson 5 (creating an error)
+3 * eggplant
+
+//Lesson 6 (Comments)
+//This is a comment that the computer will ignore
+"cake".length
+
+
