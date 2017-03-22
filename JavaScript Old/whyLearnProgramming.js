8faf3c2d1ccf6527add12ebd3ee7639ca50489a6
Lumish <mbretallen@gmail.com>
2017-03-21 22:20:19 -0400

    init

---
+++ JavaScript Old/whyLearnProgramming.js
@@ -0,0 +1,33 @@
+//Lesson 1
+//This is a comment. This will be ignored by the computer.
+"cake".length
+
+//Lesson 2
+confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
+//confirm is one of the three pop-up box creation methods.
+
+//Lesson 3
+confirm("I feel awesome!");
+confirm("I am ready to go.");
+
+//Lesson 4
+prompt("What is your name?");
+prompt("What is ubuntu?");
+prompt("Where are you from?");
+//prompt is a pop-up box creation method that creates a box that asks the user a question.
+
+//Lesson 5
+//Data comes in various types. 
+// a. numbers are quantities, just like you're used to. You can do math with them.
+/* b. strings are sequences of characters, like the letters a-z, spaces, and even numbers. 
+These are all strings: "Ryan", "4" and "What is your name?" Strings are extremely useful as labels, names, 
+and content for your programs.
+*/
+//
+"This is a string".length
+
+//Lesson 6
+//Data type 3: Booleans
+//A boolean is either true or false.
+"I'm coding like a champ".length > 10
+
