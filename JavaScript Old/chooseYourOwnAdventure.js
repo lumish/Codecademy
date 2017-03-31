77341be632abd2c31aaca14f90aae21ebd14af23
Lumish <mbretallen@gmail.com>
2017-03-30 23:52:39 -0400

    Hi

---
+++ JavaScript Old/chooseYourOwnAdventure.js
@@ -0,0 +1,105 @@
+// part 1
+// Check if the user is ready to play!
+
+confirm("Are you ready to begin your adventure?");
+
+var age = prompt("What is your age?");
+
+if(age < 13)
+{
+    console.log("Buckle up.");
+}
+else    // "otherwise"
+{
+    console.log("Let's go!");
+}
+
+// Part 2
+// Check if the user is ready to play!
+
+confirm("Are you ready to begin your adventure?");
+
+var age = prompt("What is your age?");
+
+if(age < 13)
+{
+    console.log("Buckle up.");
+}
+else    // "otherwise"
+{
+    console.log("Let's go!");
+}
+
+console.log("Suddenly, Hendrix stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race Hendrix across the street?");
+
+
+//part 3
+// Check if the user is ready to play!
+
+confirm("Are you ready to begin your adventure?");
+
+var age = prompt("What is your age?");
+
+if(age < 13)
+{
+    console.log("Buckle up.");
+}
+else    // "otherwise"
+{
+    console.log("Let's go!");
+}
+
+console.log("Suddenly, Hendrix stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race Hendrix across the street?");
+
+if(userAnswer = "yes")
+{
+    console.log("You and Hendrix start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{
+    console.log("Oh no! Hendrix shakes his head and sings 'Maybe tomorrow baby, who can say'");
+}
+
+// part 4
+// Check if the user is ready to play!
+
+confirm("Are you ready to begin your adventure?");
+
+var age = prompt("What is your age?");
+
+if(age < 13)
+{
+    console.log("Buckle up.");
+}
+else    // "otherwise"
+{
+    console.log("Let's go!");
+}
+
+console.log("Suddenly, Hendrix stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt("Do you want to race Hendrix across the street?");
+
+if(userAnswer = "yes")
+{
+    console.log("You and Hendrix start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{
+    console.log("Oh no! Hendrix shakes his head and sings 'Maybe tomorrow baby, who can say'");
+}
+
+var feedback = prompt("Do you like the game? Giving a rating out of 10.");
+
+if(feedback > 8)
+{
+    console.log("Thank you! We should race at the next concert!");
+}
+else
+{
+    console.log("I'll keep practicing coding and racing.")
+}
--- JavaScript Old/test.js
+++
@@ -1,6 +0,0 @@
-function printWordTimes(word, amount){
-	return printWordTimes(Test)
-}
-	for(var i = 0; i <= 5; i++){
-			console.log("Test");
-}
